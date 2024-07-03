bbc41772c061398055938f7ed52edf69658cc247
Gabriel Maranhão <gabrielmaranhao.cpm@gmail.com>
2024-07-03 19:28:21 -0300

    presentation-edit commit

--- src/components/sections/Presentation.js
+++ src/components/sections/Presentation.js
@@ -6,11 +6,11 @@ function Presentation(){
         <div id='presentation' className={styles.presentation}>
             <h3>Bem-vindo ao meu Portfólio</h3>
             <h1>Olá, eu sou Gabriel Maranhão!</h1>
-            <p>Sou um apaixonado por desafios e tecnologia, cresci
+            <p> Um apaixonado por desafios e tecnologia, cresci
                 ligado a computação. Estudante e profissional <br></br>
-                de TI, estou sempre procurando evoluir e entregar trabalhos com muita qualidade e dedicação, <br></br> 
-                procurando sempre inovar e gerar resultados. Meu principal projeto é o desenvolvimento de um site <br></br>
-                de eventos oficial da PUC Goiás, universidade onde sou aluno.
+                de TI, estou sempre buscando evoluir e entregar trabalhos com muita qualidade, procurando <br></br> 
+                sempre inovar e gerar resultados cada vez melhores. Atualemnte coordenador de desenvolvimento <br></br>
+                de um site de eventos oficial da PUC Goiás, universidade onde sou aluno.
             </p>
             <ButtonA text="Conecte-se comigo!!"/>
         </div>
